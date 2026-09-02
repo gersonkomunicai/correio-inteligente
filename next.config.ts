4973ee432c744fd10ca18f2961c5d442dfc4c8ae
Gerson Junior <gersonjunior2@outlook.com>
2026-09-02 17:31:57 -0300

    correção de /login e /dashboard para prevenção de erro do usuário

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,32 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
-
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/dashboard',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/campaigns',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/campaigns/manage',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  }
+}
 export default nextConfig;
